fcccf46521c74d49547dbc800f3dd58ad466f2b9
Aliko Amos <95168267+Aliko2020@users.noreply.github.com>
2023-09-21 16:17:38 +0100

    Refactor-2
    major change to code

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const body = document.body;
 const TimeBar = document.querySelector('.time')
 const greet = document.getElementById("greeting")
-const togglebtn = document.querySelector('.toggle-btn')
+// const togglebtn = document.querySelector('.toggle-btn')
 const imag = document.querySelector('.emoji')
 const imag2 = document.querySelector('.emoji2')
 const resume = document.querySelector('.resume-btn')
@@ -26,44 +26,4 @@ const linkURL = "https://drive.google.com/file/d/1-bEQwxsmf_CDojtDJwMe3sE5DYCKBO
       window.location.href = linkURL;
   
 })
-function getCurrentTimeFormatted() {
-    const now = new Date();
-    let hours = now.getHours();
-    const minutes = now.getMinutes();
-    const ampm = hours >= 12 ? 'pm' : 'am';
-  
-    // Convert hours to 12-hour format
-    hours = hours % 12;
-    hours = hours || 12; // If hours is 0, set it to 12
-  
-    // Add leading zero to minutes if needed
-    const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes;
-  
-    return `${hours}:${formattedMinutes}${ampm}`;
-  }
-  
-  function getGreetingBasedOnTime() {
-    const currentTime = new Date();
-    const currentHour = currentTime.getHours();
-  
-    if (currentHour >= 5 && currentHour < 12) {
-      return "Good morning🌄";
-    } else if (currentHour > 12 && currentHour < 17) {
-      return "Good afternoon🌞";
-    } else if(currentHour >17 && currentHour <21){
-      return "Good evening!"
-    }else
-    {
-      return "Good Night🌚";
-    }
-  }
-  
-  const currentTimeFormatted = getCurrentTimeFormatted();
-  const greeting = getGreetingBasedOnTime();
- greet.textContent = greeting
-  
-  
-
-  
-  
-  
+    
